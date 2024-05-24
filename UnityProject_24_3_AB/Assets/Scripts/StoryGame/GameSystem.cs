f7eca38b0cb988d0f4b47134eb4b9c48a96bb74e
pswxxk <sangwook1205@g.shingu.ac.kr>
2024-05-24 12:20:09 +0900

    Feat: GameSystem 랜덤 모델 불러오기

--- UnityProject_24_3_AB/Assets/Scripts/StoryGame/GameSystem.cs
+++ UnityProject_24_3_AB/Assets/Scripts/StoryGame/GameSystem.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Text;
+using STORYGAME;
 
-namespace STORYGAME
+#if UNITY_EDITOR
+[CustomEditor(typeof(GameSystem))]
+public class GameSysteEdiot : Editor       //에디터를 상속받는 클래스 생성
 {
-
-#if UNITY_EDITOR                                //전처리기 유니티 에디터에서만 동작
-    [CustomEditor(typeof(GameSystem))]
-    public class GameSysteEditor : Editor       //에디터를 상속받는 클래스 생성
-    {
         public override void OnInspectorGUI()           //유니티의 인스펙터 함수를 재정의
         {
             base.OnInspectorGUI();                      //유니티 인스펙터 함수 동작을 같이 한다. (Base)
@@ -23,39 +21,76 @@ namespace STORYGAME
                 gameSystem.ResetStroyModles();
             }
         }
-    }
+}
 #endif
-
-    public class GameSystem : MonoBehaviour
-    {
-        public static GameSystem instance;              //간단한 싱글톤 화
+public class GameSystem : MonoBehaviour
+{
+        public static GameSystem instance;
 
         private void Awake()
         {
             instance = this;
         }
 
-        public enum GAMESTATE
-        {
+        public enum GAMESTATE 
+        { 
             STORYSHOW,
             WAITSELECT,
-            STORYEND,
-            BATTLEMODE,
-            BATTLEDONE,
-            SHOPMODE,
-            ENDMODE,
+            STORYEND
         }
 
+        public Stats stats;
         public GAMESTATE currentState;
-        public StroyTableObject[] storyModels;
-        public int currentStoryInex = 1;
+        public int currentStoryIndex = 1;
+        public StoryModel[] storyModels;
+        
 
 #if UNITY_EDITOR
         [ContextMenu("Reset Story Models")]
         public void ResetStroyModles()
         {
-            storyModels = Resources.LoadAll<StroyTableObject>(""); //Resources 폴더 아래 모든 StoryModel을 불러 오기 
+            storyModels = Resources.LoadAll<StoryModel>(""); //Resources 폴더 아래 모든 StoryModel을 불러 오기 
         }
 #endif
+
+    public void StoryShow(int number)
+    {
+        StoryModel tempStoryModel = FindStoryModel(number);
+
+        //StorySystem.Instance.currentStoryModel = tempStoryModel;
+        //StroySystem.Instance.CoShowText();
+    }
+
+    StoryModel FindStoryModel(int number)
+    {
+        StoryModel tempStoryModels = null;
+        for (int i = 0; i < storyModels.Length; i++)
+        {
+            if (storyModels[i].storyNumber == number)
+            {
+                tempStoryModels = storyModels[i];
+                break;
+            }
+        }
+        return tempStoryModels;
+    }
+
+    StoryModel RandomStory()
+    {
+        StoryModel tempStoryModels = null;
+
+        List<StoryModel>storyModelList = new List<StoryModel>();
+
+        for (int i = 0; i < storyModels.Length; i++)
+        {
+            if (storyModels[i].storyType == StoryModel.STORYTYPE.MAIN)
+            {
+                storyModelList.Add(storyModels[i]);
+            }
+        }
+        tempStoryModels = storyModelList[Random.Range(0, storyModelList.Count)]; //리스트에서 랜덤으로 하나 선택
+        currentStoryIndex = tempStoryModels.storyNumber;
+        return tempStoryModels;
     }
 }
+
